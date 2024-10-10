a7e2d182832a96593f9c11aa048158af5381d5ce
Emilio González <146771552+emigzzh@users.noreply.github.com>
2024-10-10 11:29:57 -0600

    Update MainClass.cpp
    Extended functionality to option2 (fighting sim) when accesing the Pokedex and added option3 to allow users to add new pokemons to the Pokedex

--- Pokedex C++/MainClass.cpp
+++ Pokedex C++/MainClass.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 #include "DummyMaker.h"
 #include "FightingPokemon.h"
@@ -64,6 +65,15 @@ int DataLoader(MainClass& vectorList){
     return 0;
 }
 
+bool esTipoValido(string tipo) {
+    string tiposValidos[] = {"Normal", "Fire", "Water", "Electric", "Grass", 
+                             "Ice", "Fighting", "Poison", "Ground", "Flying", 
+                             "Psychic", "Bug", "Rock", "Ghost", "Dragon", 
+                             "Dark", "Steel", "Fairy"};
+
+    return find(begin(tiposValidos), end(tiposValidos), tipo) != end(tiposValidos);
+}
+
 int main(){
     MainClass list;
     DataLoader(list);
@@ -75,7 +85,7 @@ int main(){
 
     while(flag) {
         cout << "\n <1> Mostrar las estadísticas de algún Pokémon" << endl;
-        cout << " <2> Comparar los tipos de dos Pokemones" << endl;
+        cout << " <2> Simular un combate entre dos Pokemones" << endl;
         cout << " <3> Ingresar un nuevo Pokemón a la Pokedex" << endl;
         cin >> option;
 
@@ -133,8 +143,11 @@ int main(){
                             bool flagCase2_1 = true;
 
                             while(flagCase2_1){
+                                cout << "Inserte el nivel: ";
                                 cin >> level;
+                                cout << "Inserte los puntos EVs (1 -> 540): ";
                                 cin >> evs;
+                                cout << "Inserte los puntos IVs (1 -> 31): ";
                                 cin >> ivs;
 
                                 if(level > 0 && evs > 0 && ivs > 0){
@@ -175,8 +188,11 @@ int main(){
                             bool flagCase2_1 = true;
 
                             while(flagCase2_1){
+                                cout << "Inserte el nivel: ";
                                 cin >> level;
+                                cout << "Inserte los puntos EVs (1 -> 540): ";
                                 cin >> evs;
+                                cout << "Inserte los puntos IVs (1 -> 31): ";
                                 cin >> ivs;
 
                                 if(level > 0 && evs > 0 && ivs > 0){
@@ -201,47 +217,163 @@ int main(){
 
                 int attackerTypeOption;
                 string attackerType;
-                cout << "Su Pokemon tiene los siguientes dos tipos, seleccione cuál usará para atacar (si solo hay uno, se seleccionará ese automáticamente)" << endl;
+                cout << "\nSu Pokemon tiene los siguientes dos tipos, seleccione cuál usará para atacar (si solo hay uno, se seleccionará ese automáticamente)" << endl;
                 cout << " <1>" << attacker.getType1() << endl;
                 cout << " <2>" << attacker.getType2() << endl;
                 cin >> attackerTypeOption;
 
-                if(attackerTypeOption == 2 && !(attacker.getType2().empty())) {
+                if(attackerTypeOption == 2 && attacker.getType2() != "NONE") {
                     attackerType = attacker.getType2();
                 } else {
                     attackerType = attacker.getType1();
                 }
 
                 cout << "\n\n>> Comienza la pelea entre " << attacker.getName() << " y " << defender.getName() << endl;
-                bool endFight = false;
+                attacker.printPokemon();
+                defender.printPokemon();
 
-                while(!endFight) {
+                while(true) {
                     string defenderType;
 
-                    if(rand()%2 == 0) { defenderType = defender.getType1(); }
+                    if(rand()%2 == 0 || defender.getType2() == "NONE") { defenderType = defender.getType1(); }
                     else { defenderType = defender.getType2(); }
 
-                    cout << "\n" << attacker.getName() << " ataca a " << defender.getName() << endl;
+                    cout << "\n" << attacker.getName() << " ataca a " << defender.getName() << " con un ataque tipo <" << attackerType << ">" << endl;
                     defender.damagePokemon(attackerType, attacker);
 
-                    if(defender.getHp() <= 0 && !endFight) {
+                    if(defender.getHp() <= 0) {
                         cout << "\n>> " << attacker.getName() << " es el ganador! Con " << attacker.getHp() << " HP restante" << endl;
-                        endFight = true;
+                        break;
                     }
 
-                    cout << "\n" << defender.getName() << " ataca a " << attacker.getName() << endl;
+                    cout << "\n" << defender.getName() << " ataca a " << attacker.getName() << " con un ataque tipo <" << defenderType << ">" << endl;
                     attacker.damagePokemon(defenderType, defender);
 
-                    if(attacker.getHp() <= 0 && !endFight) {
+                    if(attacker.getHp() <= 0) {
                         cout << "\n>> " << defender.getName() << " es el ganador! Con " << defender.getHp() << " HP restante" << endl;
-                        endFight = true;
+                        break;
                     }
                 }
                 
                 break;
             }
             case 3: {
+                cout << "\nInserte los siguientes parámetros para crear un nuevo Pokemon" << endl;
+                string name, generation, status, type1, type2;
+                double height, weight;
+                int hp, defense, speed;
 
+                cout << "Nombre: " << endl;
+                cin >> name;
+
+                cout << "\nGeneración (Inserte un número): " << endl;
+                cin >> generation;
+
+                cout << "\nRareza: " << endl;
+                cin >> status;
+
+                bool validInput = false;
+                while (!validInput) {
+                    cout << "\nTipo1 del Pokemon: " << endl;
+                    cin >> type1;
+
+                    if (esTipoValido(type1)) {
+                        validInput = true;
+                    } else {
+                        cout << "> Error: Ingrese un tipo válido para Tipo1" << endl;
+                    }
+                }
+
+                validInput = false;
+                cout << "\nTipo2 del Pokemon (Opcional, puede dejarlo vacío si inserta 0): " << endl;
+                cin >> type2;
+
+                if (type2 != "0") {
+                    while (!validInput) {
+                        if (esTipoValido(type2)) {
+                            validInput = true;
+                        } else {
+                            cout << "> Error: Ingrese un tipo válido para Tipo2, o inserte un 0 si no desea asignar un Tipo2" << endl;
+                            cin >> type2;
+                            if (type2 == "0") {
+                                type2 = "NONE";
+                                validInput = true;
+                            }
+                        }
+                    }
+                } else {
+                    type2 = "NONE";
+                }
+
+                validInput = false;
+                while (!validInput) {
+                    cout << "\nAltura (en m, sin insertar la unidad): " << endl;
+                    cin >> height;
+                    if (cin.fail() || height <= 0) { 
+                        cout << "> Error: Ingrese un valor numérico válido para la altura.\n" << endl;
+                        cin.clear();
+                        cin.ignore(1000, '\n');
+                    } else {
+                        validInput = true;
+                    }
+                }
+
+                validInput = false;
+                while (!validInput) {
+                    cout << "\nPeso (en kg, sin insertar la unidad): " << endl;
+                    cin >> weight;
+                    if (cin.fail() || weight <= 0) {
+                        cout << "> Error: Ingrese un valor numérico válido para el peso.\n" << endl;
+                        cin.clear();
+                        cin.ignore(1000, '\n');
+                    } else {
+                        validInput = true;
+                    }
+                }
+
+                validInput = false;
+                while (!validInput) {
+                    cout << "\nHitPoints (HP): " << endl;
+                    cin >> hp;
+                    if (cin.fail() || hp <= 0) {
+                        cout << "> Error: Ingrese un valor numérico válido para los HP.\n" << endl;
+                        cin.clear();
+                        cin.ignore(1000, '\n');
+                    } else {
+                        validInput = true;
+                    }
+                }
+
+                validInput = false;
+                while (!validInput) {
+                    cout << "\nPuntos de defensa: " << endl;
+                    cin >> defense;
+                    if (cin.fail() || defense < 0) {
+                        cout << "> Error: Ingrese un valor numérico válido para la defensa.\n" << endl;
+                        cin.clear();
+                        cin.ignore(1000, '\n');
+                    } else {
+                        validInput = true;
+                    }
+                }
+
+                validInput = false;
+                while (!validInput) {
+                    cout << "\nPuntos de velocidad: " << endl;
+                    cin >> speed;
+                    if (cin.fail() || speed < 0) {
+                        cout << "> Error: Ingrese un valor numérico válido para la velocidad.\n" << endl;
+                        cin.clear();
+                        cin.ignore(1000, '\n');
+                    } else {
+                        validInput = true;
+                    }
+                }
+
+                DummyMaker dummy(name, generation, status, type1, type2, height, weight, hp, defense, speed);
+                list.addItem(dummy);
+
+                dummy.printPokemon();
                 break;
             }
             default: {
